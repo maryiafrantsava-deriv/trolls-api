16777c65c6bf4dc1ca4cc7e0063a9e30f44242c0
amina-deriv <amina@deriv.com>
2021-11-11 13:37:54 +0400

    initial commit

---
+++ components/common/Header/header.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import type { NextFunctionComponent } from 'next'
+
+
+// const Header: NextFunctionComponent<Props> = ()=>{
+//     return <span>Hello </span>
+// }
+
+function Header(){
+return <span>Hello </span>
+}
+
+export default Header
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/common/Header/header'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Deriv.com API</title>
+        <meta name="description" content="Build your own trading platform, powered by the Deriv API. We use WebSockets for fast, two-way messaging between your apps and our trading services." />
+        <link rel="icon" href="/deriv.png" />
+      </Head>
+   <Header/>
+      
+    </div>
+  )
+}
+
+export default Home
