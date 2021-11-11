20afa57f18dd09ba7690ccf6a0c15cf9d63b444c
amina-deriv <amina@deriv.com>
2021-11-11 15:00:01 +0400

    comments removed

--- components/common/Header/header.tsx
+++ components/common/Header/header.tsx
@@ -1,10 +1,4 @@
 import React from 'react'
-// import type { NextFunctionComponent } from 'next'
-
-
-// const Header: NextFunctionComponent<Props> = ()=>{
-//     return <span>Hello </span>
-// }
 
 const Header:React.FC=()=>{
 return <span>Hello </span>
