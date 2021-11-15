6ebf772b0bf9b62b41a4328ebc5eb8a0d7b91efc
Maryia Frantsava <maryia.frantsava@deriv.com>
2021-11-15 20:58:32 +0300

    MaryiaF/fix: the indent of "comprehensive" component

--- src/components/Home/Sections/Comprehensive/Comprehensive.tsx
+++ src/components/Home/Sections/Comprehensive/Comprehensive.tsx
@@ -10,23 +10,25 @@ type ComprehensivePropsType = {
 const Comprehensive: React.FC<ComprehensivePropsType> = ({id}) => {
 
     return id === 'comprehensive' ? (
-        <div className={`${styles.rowContainer} ${styles.gray}`}>
-            <div className={`${styles.singleContainer}`}>
-                    <img
-                        className={`${styles.libraryIcon}`}
-                        src={dataComprehensive.icon}
-                        width={64}
-                        height={64}
-                        alt="preview"
-                    />
-                    <Title headerSize='h1'>{dataComprehensive.title}</Title>
-                    <div className={`${styles.subheader}`}>
-                        <span>{dataComprehensive.textFirstPart}</span>
-                        <span>{dataComprehensive.textSecondPart}</span>
+        <section className={`${styles["main-page-row"]}`}>
+                <div className={`${styles.rowContainer}`}>
+                    <div className={`${styles.singleContainer}`}>
+                            <img
+                                className={`${styles.libraryIcon}`}
+                                src={dataComprehensive.icon}
+                                width={64}
+                                height={64}
+                                alt="preview"
+                            />
+                            <Title headerSize='h1'>{dataComprehensive.title}</Title>
+                            <div className={`${styles.subheader}`}>
+                                <span>{dataComprehensive.textFirstPart}</span>
+                                <span>{dataComprehensive.textSecondPart}</span>
+                            </div>
+                            {dataComprehensive.link}
                     </div>
-                    {dataComprehensive.link}
-            </div>
-        </div>
+                </div>
+        </section>
     ) : null;
 }
 
