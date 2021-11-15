3b5a133e118324893d8b1cef97e4737817db97ca
george-usynin-deriv <heorhi@deriv.com>
2021-11-15 18:54:52 +0300

    George/removed Section Component, refactor classNames

--- src/components/FeedbackSection/SingleFeedbackSection/SingleFeedbackSection.tsx
+++ src/components/FeedbackSection/SingleFeedbackSection/SingleFeedbackSection.tsx
@@ -3,42 +3,31 @@ import Link from "next/link";
 import styles from "./SingleFeedbackSection.module.scss";
 
 type SingleFeedbackSectionPropsType = {
-  headerSize: number;
-  title: string;
-  description?: Array<string | JSX.Element>;
-  buttonLinkPath?: string;
-  titleButton?: string;
-  showButton: boolean;
-  background: "black" | "gray"
-
+    headerSize: number;
+    title: string;
+    description?: Array<string | JSX.Element>;
+    buttonLinkPath?: string;
+    titleButton?: string;
+    showButton: boolean;
+    background: "black" | "gray";
 };
 
-const SingleFeedbackSection: React.FC<SingleFeedbackSectionPropsType> = (
-    props
-) => {
-    const {
-        title,
-        headerSize,
-        description,
-        buttonLinkPath,
-        titleButton,
-        showButton,
-        background,
-    } = props;
+const SingleFeedbackSection: React.FC<SingleFeedbackSectionPropsType> = props => {
+    const { title, headerSize, description, buttonLinkPath, titleButton, showButton, background } = props;
 
     const CustomHeaderTitleTag = `h${headerSize}` as keyof JSX.IntrinsicElements;
 
-    const titleButtonTextColor = background === "black" ? styles.whiteColor : styles.blackColor
-    const paragraphColor = background === "black" ? styles.grayColor : styles.blackColor
+    const titleButtonTextColor = background === "black" ? styles.whiteColor : styles.blackColor;
+    const paragraphColor = background === "black" ? styles.grayColor : styles.blackColor;
 
     return (
-        <div className={styles.singleContainer}>
+        <div className={`${styles["single-container"]}`}>
             <CustomHeaderTitleTag className={titleButtonTextColor}>{title}</CustomHeaderTitleTag>
             {description && <p className={paragraphColor}>{description}</p>}
             {showButton && buttonLinkPath && (
                 <Link href={buttonLinkPath}>
                     <a
-                        className={`${styles.communityButton} ${titleButtonTextColor}`}
+                        className={`${styles["community-btn"]} ${titleButtonTextColor}`}
                         target="_blank"
                         rel="noreferrer noopener"
                     >
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,12 @@
 import FeedbackSection from "../components/FeedbackSection/FeedbackSection";
-import Section from "../components/Home/Section/Section";
-import { BenefitIcon } from "../components/BenefitsSection/BenefitIcon/BenefitIcon";
-import { benefitIconsArray } from "../utils/content";
-import styles from "../styles/Home.module.scss";
+import { BenefitSection } from "../components/BenefitsSection/BenefitSection";
 
 const Home = () => {
     return (
         <div>
             Good luck Trolls!
-            <Section>
-                <FeedbackSection id="home" />
-            </Section>
-            <Section
-                title={"Benefits of using Deriv API"}
-                sectioClassName={styles.mainBenefitSection}
-                childrenClassname={styles.benefitColumnContainer}
-            >
-                <div className={styles.benefitIconsContainer}>
-                    {benefitIconsArray.map((el, index) => {
-                        return <BenefitIcon title={el.title} imgPath={el.imgPath} key={index} />;
-                    })}
-                </div>
-            </Section>
+            <BenefitSection />
+            <FeedbackSection id="home" />
         </div>
     );
 };
