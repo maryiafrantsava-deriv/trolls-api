b385730d2c820f61510e17123c3e55c6f8284b0b
Farrah Mae Ochoa <farrah@deriv.com>
2021-11-16 10:19:55 +0400

    fix faq section in responsive mode

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Footer: React.FC = () => <></>;
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="footer-container">© 2021 Deriv | All rights reserved</div>
+        </footer>
+    );
+};
 
 export default Footer;
