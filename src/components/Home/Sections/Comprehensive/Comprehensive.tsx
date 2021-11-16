c2c63a14402d39ad28a9d082d3e584fdc822d70d
Maryia Frantsava <maryia.frantsava@deriv.com>
2021-11-16 12:34:06 +0300

    MaryiaF/refactor: components of home page (Comprehensive, DerivApi, TextContent), styles

--- src/components/Home/Sections/Comprehensive/Comprehensive.tsx
+++ src/components/Home/Sections/Comprehensive/Comprehensive.tsx
@@ -1,42 +1,27 @@
 import React from "react";
 import data_comprehensive from "utils/data-comprehensive";
-import styles from "../Comprehensive/Comprehensive.module.scss";
-import Title from "components/common/Title/Title";
-import data_deriv_api from "utils/data-deriv-api";
+import styles from "./Comprehensive.module.scss";
 import Image from "next/image";
+import styleSections from "../Sections.module.scss";
+import TextContent from "../TextContent/TextContent";
 
-type ComprehensivePropsType = {
-    id: string;
-};
-
-const Comprehensive: React.FC<ComprehensivePropsType> = ({ id }) => {
-    return id === "comprehensive" ? (
-        <section className={`${styles["main-page-row"]}`}>
-            <div className={`${styles.rowContainer}`}>
-                <div className={`${styles.singleContainer}`}>
-                    <div className={`${styles.libraryIcon}`}>
-                        <Image src={data_comprehensive.icon} width={64} height={64} alt="preview" />
-                    </div>
-                    <Title headerSize="h1">{data_comprehensive.title}</Title>
-                    <div className={`${styles.subheader}`}>
-                        <span>{data_comprehensive.textFirstPart}</span>
-                        <span>{data_comprehensive.textSecondPart}</span>
-                    </div>
-                    {data_comprehensive.link}
+const Comprehensive: React.FC = () => (
+    <section className={`${styleSections["main-page-row"]}`}>
+        <div className={`${styles.rowContainer}`}>
+            <div className={`${styles.singleContainer}`}>
+                <div className={`${styles.libraryIcon}`}>
+                    <Image
+                        src={data_comprehensive.icon}
+                        width={64}
+                        height={64}
+                        alt="preview"
+                    />
                 </div>
-            </div>
-        </section>
-    ) : (
-        <div className={`${styles["hero-image"]}`}>
-            <div className={`${styles["hero-container"]}`}>
-                <Title headerSize="h1">{data_deriv_api.title}</Title>
-                <div className={`${styles.subheader}`}>
-                    <span>{data_deriv_api.textFirstPart}</span>
-                    <span>{data_deriv_api.textSecondPart}</span>
-                </div>
+                <TextContent data={data_comprehensive}/>
+                {data_comprehensive.link}
             </div>
         </div>
-    );
-};
+    </section>
+)
 
 export default Comprehensive;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import FeedbackSection from "../components/FeedbackSection/FeedbackSection";
 import Comprehensive from "../components/Home/Sections/Comprehensive";
 import { BenefitSection } from "../components/BenefitsSection/BenefitSection";
+import DerivApi from "components/Home/Sections/DerivApi/DerivApi";
 
 const Home = () => {
     return (
         <div>
-            <Comprehensive id="derivApi" />
+            <DerivApi id="derivApi" />
             <BenefitSection />
             <Comprehensive id="comprehensive" />
             <FeedbackSection id="home" />
