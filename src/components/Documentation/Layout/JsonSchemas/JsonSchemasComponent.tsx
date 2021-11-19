e9c93ecbe6a0b488c804f0fd95595bc0a11693ba
Mikita Masliyevich <83015762+mikita-deriv@users.noreply.github.com>
2021-11-19 11:01:50 +0300

    Mikita / feat: add JSON Schemas module (#22)
    * feat: add jsx
    
    * feat: fixed EsLint errors
    
    * feat: add JSON Schemas module
    
    * feat: add Link for all navigation
    
    * feat: removed repeated styles
    
    * feat: removed fragments
    
    Co-authored-by: Mikit Masliyevich <mikitmasliyevich@Mikits-MacBook-Pro.local>

---
+++ src/components/Documentation/Layout/JsonSchemas/JsonSchemasComponent.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import style from "./JsonSchemasComponent.module.scss"
+import Link from "next/link";
+
+
+const JsonSchemasComponent: React.FC = () => {
+    
+    const json_navigation = [
+        { id: "json-schemas", title: "JSON Schemas", path: "https://github.com/binary-com/websockets/tree/gh-pages/config" },
+        { id: "changelog", title: "changelog", path: "https://github.com/binary-com/websockets/commits/gh-pages" },
+        { id: "json-editor", title: "JSON Editor with JSON Schema support", path: "https://jeremydorn.com/json-editor/" },
+        { id: "json-schema_ts", title: "Generate JSON Schema from TypeScript", path: "https://lbovet.github.io/typson-demo/" },
+        { id: "json-schema_object", title: "Generate JSON Schema from JSON Object", path: "https://jsonschema.net/" },
+        { id: "json-schema_validator", title: "JSON Schema Validator", path: "https://www.jsonschemavalidator.net/" },
+    ];
+
+    return(
+        <div className={style["page-container"]}>
+            <h1 className="doc-main-title">JSON Schemas</h1>
+            <p>
+     Our API is defined by 
+                <Link href={json_navigation[0].path}><a target="_blank" rel="noreferrer"> {json_navigation[0].title}</a></Link>. 
+        Get updates by looking for JSON Schema Update in the 
+                <Link href={json_navigation[1].path}><a target="_blank" rel="noreferrer"> {json_navigation[1].title}</a></Link>. 
+            </p>
+            <h3>Useful tools:</h3>
+            {json_navigation.map(
+                ({ id, title, path }, i) =>
+                    i > 1 && (
+                        <p>
+                            <Link key={id} href={path}>
+                                <a target="_blank" rel="noreferrer">{title}</a>
+                            </Link>
+                        </p>
+                    )
+            )}
+        </div>
+    );
+};
+
+
+export default JsonSchemasComponent;
--- src/pages/docs/json-schemas.tsx
+++ src/pages/docs/json-schemas.tsx
@@ -1,8 +1,12 @@
 import { PageComponentWithLayout } from "../../types";
-import DocsLayout from "../../components/Documentation/Layout/DocsLayout";
+import DocsLayout from "components/Documentation/Layout/DocsLayout";
+import JsonSchemasComponent from "components/Documentation/Layout/JsonSchemas/JsonSchemasComponent"
+
 
 const JSONSchemas: PageComponentWithLayout = () => {
-    return <div>JSON Schemas</div>;
+
+    return <JsonSchemasComponent/>
+    
 };
 
 JSONSchemas.Layout = DocsLayout;
