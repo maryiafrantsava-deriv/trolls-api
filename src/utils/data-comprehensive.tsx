dd02c7a71d5439420adb38b054b0460f8c42612e
Farrah Mae Ochoa <farrah@deriv.com>
2021-11-23 18:02:12 +0400

    fix comprehensive text

--- src/utils/data-comprehensive.tsx
+++ src/utils/data-comprehensive.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 const data_comprehensive = {
     icon: "/js-library.svg",
-    title: [ "Comprehensive all-in-one JavaScript client library" ],
-    textFirstPart: [ "Simplify your development processes and get your app up and running" ],
-    textSecondPart: [ "faster with our all-in-one client library" ],
-    link: [ <Link href={"https://github.com/binary-com/deriv-api"} key="js-library">
-        <a rel="noreferrer noopener">Take me to the library</a>
-    </Link> ]
+    title: ["Comprehensive all-in-one JavaScript client library"],
+    textFirstPart: [
+        "Simplify your development processes and get your app up and running faster with our all-in-one client library",
+    ],
+    textSecondPart: ["faster with our all-in-one client library"],
+    link: [
+        <Link href={"https://github.com/binary-com/deriv-api"} key="js-library">
+            <a rel="noreferrer noopener">Take me to the library</a>
+        </Link>,
+    ],
 };
 
 export default data_comprehensive;
