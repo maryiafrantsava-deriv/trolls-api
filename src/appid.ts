4ef47532ba12887497fad2eaa6a868912051174c
Maryia <87321516+maryia-deriv@users.noreply.github.com>
2021-11-24 21:09:08 +0300

    Revert "Maryia/isolated buttons block, added Reset button logics & improved NotFound(404) page (#65)"
    This reverts commit 909f96da77aa63fc21f2a377f00a380930b0224d.

--- src/appid.ts
+++ src/appid.ts
@@ -1,11 +1,11 @@
 // @ts-ignore
-import DerivAPIBasic from "@deriv/deriv-api/dist/DerivAPIBasic";
+import DerivAPIBasic from '@deriv/deriv-api/dist/DerivAPIBasic';
 
 const isBrowser = typeof window !== "undefined";
 
-export const generateDerivApiInstance = () => {
+const generateDerivApiInstance = () => {
     if (!isBrowser) return;
-
+    
     const server = "frontend.binaryws.com";
     const language = "EN";
     const app_id = "11780";
@@ -13,12 +13,10 @@ export const generateDerivApiInstance = () => {
 
     const socket_url = `wss://${server}/websockets/v3?app_id=${app_id}&l=${language}&brand=${brand_name}`;
     const deriv_socket = new WebSocket(socket_url);
-
-    const api = new DerivAPIBasic({
+    
+    return new DerivAPIBasic({
         connection: deriv_socket,
     });
-
-    return api;
 };
 
 export const api = generateDerivApiInstance();
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,9 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 const NotFound = () => {
-    const router = useRouter();
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            router.push("/");
-        }, 4000);
-        return () => {
-            clearTimeout(timeoutId);
-        };
-    }, [router]);
-
     return (
-        <div className="not_found-wrapper">
-            <div>404: Not Found</div>
-            <div>Something is wrong...</div>
+        <div>
+        404: Not Found
         </div>
     );
 };
-
+  
 export default NotFound;
