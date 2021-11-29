548fff175224a7443de67d3eaa76165e87616117
Carol Sachdeva <carol@binary.com>
2021-11-29 14:49:02 +0800

    fix: comprehensive text + styles

--- src/utils/data-comprehensive.tsx
+++ src/utils/data-comprehensive.tsx
@@ -6,7 +6,7 @@ const data_comprehensive = {
     textFirstPart: [
         "Simplify your development processes and get your app up and running faster with our all-in-one client library",
     ],
-    textSecondPart: ["faster with our all-in-one client library"],
+    textSecondPart: [],
     link: [
         <Link href={"https://github.com/binary-com/deriv-api"} key="js-library">
             <a rel="noreferrer noopener">Take me to the library</a>
