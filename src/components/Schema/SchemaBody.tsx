1df7d353830c8432a578ca0dc2edf329b94f4fef
“Larkgronick” <larkgronick@gmail.com>
2021-11-29 09:52:32 +0300

    feat: create base for side info components

---
+++ src/components/Schema/SchemaBody.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import style from "./Schema.module.scss";
+
+type SchemaBodyProps = {
+    properties: object;
+    description: string;
+}
+
+const Properities: React.FC<SchemaBodyProps> = ({properties}) => {
+    const names = Object.keys(properties);
+
+    const CodeString: React.FC<SchemaBodyProps> = ({description}) => {
+        const highlightCode = description.split(' ').map((el,i) => {
+            return (/`([^`]*)`/.test(el)) ?
+                <span
+                    className={`${style["schema-role"]} ${style["schema-code"]}`}
+                    key={el + i}
+                >{`${el.slice(1, el.length - 1)}`}
+                </span>
+                : ` ${el} `;
+        });
+        return (
+            <div className={`${style["schema-body-description"]}`}>{highlightCode}</div>
+        );
+    }
+
+    return (
+        <div>{names.map(el => {
+            const {type, description, pattern, enum: _enum, items} = properties[el];
+
+            return (
+                <div className={`${style["schema-body-signature"]}`}>
+                    <div className={`${style["schema-body-header"]}`}>
+                        <p><strong>{el}</strong></p>
+                        {_enum ? <div className={`${style["schema-body-type"]}`}>{type}
+                            <div className={`${style["schema-enums"]}`}>{_enum.map((el, i) => <div
+                                className={`${style["schema-role"]} ${style["schema-code"]} ${style["schema-enums"]}`}
+                                key={el + i}>{el}</div>)}
+                            </div>
+                        </div> : null}
+                        {pattern ? <div className={`${style["schema-regex-container"]}`}>
+                            <div className={`${style["schema-pattern-type"]}`}>{type}</div>
+                            <div className={`${style["schema-body-pattern"]}`}>{pattern}</div>
+                        </div> : null}
+                    </div>
+                    <CodeString description={description}/>
+                </div>
+            );
+        })}
+
+        </div>
+    )
+}
+
+
+const SchemaBody: React.FC<SchemaBodyProps> = ({properties}) => {
+ return (
+     <div className={`${style["schema-body"]}`}>
+         <Properities properties={properties}/>
+     </div>
+ )
+}
+
+export default SchemaBody;
--- src/components/Schema/SchemaWrapper.tsx
+++ src/components/Schema/SchemaWrapper.tsx
@@ -1,28 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import SchemaHeader from  "./SchemaHeader";
+import SchemaBody from "./SchemaBody";
 import Title from "../common/Title/Title";
 import send from "../../utils/send";
 import receive from "../../utils/receive"
-import style from "./SchemaWrapper.module.scss"
+import style from "./Schema.module.scss"
 
 type SchemaWrapperProps = {
     info: any;
 }
 
 const SchemaWrapper:React.FC<SchemaWrapperProps> = ({info}) => {
+    const {title, description, auth_required, auth_scopes, properties } = info;
 
-    console.log(info)
-    if(info){
-        return (
-            <div className={`${style["page-wrapper"]} ${style.dark}`}>
-                <Title className="" headerSize={"h3"}>{info.title}</Title>
-                <p>{info.description}</p>
-            </div>
-        )
-    } else {
-        return null
-    }
-
-
+    return (Object.entries(info).length !== 0) ?
+        (
+            <>
+                <SchemaHeader
+                    title={title}
+                    description={description}
+                    auth_required={auth_required}
+                    auth_scopes={auth_scopes} />
+                <SchemaBody
+                    properties={properties}
+                />
+            </>
+        ) : null;
 }
 
 export default SchemaWrapper;
